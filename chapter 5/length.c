98e68ef8c996cfcf286abb21328f46cea0036df7
Jonathan Moore <jonathan.moore6247@gmail.com>
2023-01-27 14:00:04 -0500

    Added string length program

---
+++ chapter 5/length.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdio.h>
+
+char line[100]; /* line we are looking at */
+
+int main() {
+    printf("Enter a line: "); 
+    fgets(line, sizeof(line), stdin);
+
+    printf("The length of the line is: %d\n", strlen(line));
+    return 0;
+}
