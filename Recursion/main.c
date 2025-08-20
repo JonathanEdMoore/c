b69d6fc691fdffbcc161b2ab052f11274267921a
Jonathan Moore <jonathan.moore6247@gmail.com>
2025-08-20 11:05:06 -0400

    Nested recursion

--- Recursion/main.c
+++ Recursion/main.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
-void funB(int n);
 
-void funA(int n) {
-   if (n > 0) {
-    printf("%d\n", n);
-    funB(n - 1);
-   } 
-}
-
-void funB(int n) {
-    if (n > 0) {
-        printf("%d\n", n);
-        funA(n/2);
+int fun(int n) {
+    if (n > 100) {
+        return n - 10;
+    }
+    else {
+        return fun(fun(n + 11));
     }
 }
 
-
 int main() {
-    funA(20);
+    int r;
+    r = fun(95);
+    printf("%d\n", r);
     return 0;
 }
