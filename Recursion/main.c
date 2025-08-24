1df804df6af48d71b264adc43089496ce230906e
Jonathan Moore <jonathan.moore6247@gmail.com>
2025-08-24 15:19:23 -0400

    Exponents iterattion

--- Recursion/main.c
+++ Recursion/main.c
@@ -1,17 +1,75 @@
 #include <stdio.h>
 
-int fun(int n) {
-    if (n > 100) {
-        return n - 10;
+int sum(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return sum(n - 1) + n;
+}
+
+int Isum(int n) {
+    int s = 0;
+    int i;
+    for(i = 1; i <= n; i++) {
+        s = s + i;
+    }
+    return s;
+}
+
+int fact(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return fact(n - 1) * n;
+}
+
+int Ifact(int n) {
+    int result = 1;
+    int i;
+
+    for (i = 1; i <= n; i++) {
+        result = result * i;   
+    }
+
+    return result;
+}
+
+int pow(int m, int n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    if (n % 2 == 0) {
+        return pow(m * m, n/2);
     }
     else {
-        return fun(fun(n + 11));
+        return m * pow(m * m, (n - 1)/2);
     }
+}
+
+int Ipow(int m, int n) {
+    int result = 1;
+    int exp = n;
+
+    if (exp < 0) {
+        m = 1 / m;
+        exp = -exp;
+    }
+
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result *= m;
+        }
+        m *= m;
+        exp /= 2;
+    }
+
+    return result;
 }
 
 int main() {
     int r;
-    r = fun(95);
-    printf("%d\n", r);
+    r = Ipow(2, 5);
+    printf("%d", r);
     return 0;
 }
